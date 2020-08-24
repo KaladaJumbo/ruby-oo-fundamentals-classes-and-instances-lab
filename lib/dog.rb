264f860a9feaa459d9a8259f1152be21a89284b5
KaladaJumbo <wkalada1@gmail.com>
2020-08-24 16:26:15 -0400

    finished lab v1

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class dog 
+
+    #intentionally left blank
+
+end
+
+fido = dog.new
+snoopy = dog.new 
+lassie = dog.new
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class person 
+
+    #intentionally left blank
+
+end
+
+adele_goldberg = person.new 
+alan_kay = person.new
